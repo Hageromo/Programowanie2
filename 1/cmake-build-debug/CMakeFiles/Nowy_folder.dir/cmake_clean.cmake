31067df334a5b7a2da450b1b817e49819e44aa25
Hageromo <44583424+Hageromo@users.noreply.github.com>
2019-03-24 09:43:08 +0100

    Add files via upload

---
+++ 1/cmake-build-debug/CMakeFiles/Nowy_folder.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nowy_folder.dir/main.cpp.obj"
+  "Nowy_folder.pdb"
+  "Nowy_folder.exe"
+  "Nowy_folder.exe.manifest"
+  "libNowy_folder.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nowy_folder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
